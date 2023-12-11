c41596b2f4d7ca4988931c6a992f5fd066fae85c
蓝天 <eyjian@qq.com>
2023-12-11 22:58:16 +0800

    add comments

--- gateway_login/authentication/internal/logic/authenticatelogic.go
+++ gateway_login/authentication/internal/logic/authenticatelogic.go
@@ -28,13 +28,15 @@ func NewAuthenticateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Auth
 func (l *AuthenticateLogic) Authenticate(in *auth.AuthReq) (*auth.AuthResp, error) {
 	// todo: add your logic here and delete this line
 	fmt.Printf("SessionId: %s\n", in.SessionId)
-	if in.SessionId == "2023121200" {
-		authResp := &auth.AuthResp {
+	if in.SessionId == "2023121200" { // 实际场景可能是：以 in.SessionId 为 key，从 redis 读取会话的数据
+		// 通过鉴权
+		authResp := &auth.AuthResp{
 			UserId: "10001",
 		}
 		fmt.Printf("UserId: %s\n", authResp.UserId)
 		return authResp, nil
 	} else {
+		// 未通过鉴权
 		fmt.Printf("no perm: %s\n", in.SessionId)
 		return nil, errors.New("No perm in auth.Authenticate")
 	}
